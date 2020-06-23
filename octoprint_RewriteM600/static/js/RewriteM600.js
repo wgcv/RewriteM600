b8155c64f1c86283dac44f17d4eac54ab842e096
Gustavo Cevallos <gstavocevallos@gmail.com>
2020-06-23 11:45:55 -0700

    first commit

---
+++ octoprint_RewriteM600/static/js/RewriteM600.js
@@ -0,0 +1,29 @@
+/*
+ * View model for RewriteM600
+ *
+ * Author: Gustavo Cevallos
+ * License: MIT
+ */
+$(function() {
+    function Rewritem600ViewModel(parameters) {
+        var self = this;
+
+        // assign the injected parameters, e.g.:
+        // self.loginStateViewModel = parameters[0];
+        // self.settingsViewModel = parameters[1];
+
+        // TODO: Implement your plugin's view model here.
+    }
+
+    /* view model class, parameters for constructor, container to bind to
+     * Please see http://docs.octoprint.org/en/master/plugins/viewmodels.html#registering-custom-viewmodels for more details
+     * and a full list of the available options.
+     */
+    OCTOPRINT_VIEWMODELS.push({
+        construct: Rewritem600ViewModel,
+        // ViewModels your plugin depends on, e.g. loginStateViewModel, settingsViewModel, ...
+        dependencies: [ /* "loginStateViewModel", "settingsViewModel" */ ],
+        // Elements to bind to, e.g. #settings_plugin_RewriteM600, #tab_plugin_RewriteM600, ...
+        elements: [ /* ... */ ]
+    });
+});
